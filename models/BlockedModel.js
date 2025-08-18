6625e94b3780e420832706728579b91e492d3d56
bond0010732 <122272180+bond0010732@users.noreply.github.com>
2025-08-18 13:34:54 +0100

    Update BlockedModel.js

--- models/BlockedModel.js
+++ models/BlockedModel.js
@@ -3,20 +3,12 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const unlockSchema = new Schema({
-  //    blocker: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-  // blocked: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-  // blockedAt: { type: Date, default: Date.now }
-      userA: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-  userB: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-  unlockedBy: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true }, // who paid
-  cost: { type: Number, default: 0 },
-  unlockedAt: { type: Date, default: Date.now },
+const blockedUserSchema = new Schema({
+     blocker: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+  blocked: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+  blockedAt: { type: Date, default: Date.now },
     // Add other fields as needed
 });
 
-// const blockedUser = mongoose.model('BlockedFriends', blockedUserSchema);
-// module.exports = blockedUser;
-
-const UnlockAccess = mongoose.model('UnlockAccess', unlockSchema);
-module.exports = UnlockAccess;
+const blockedUser = mongoose.model('BlockedFriends', blockedUserSchema);
+module.exports = blockedUser;
