2b9e2a0914dcc92225610965f215bd546c2c3484
bond0010732 <122272180+bond0010732@users.noreply.github.com>
2025-08-18 14:18:51 +0100

    Update server.js

--- server.js
+++ server.js
@@ -526,7 +526,7 @@ socket.on('sendMessage', async ({ roomId, msg }) => {
       ],
     });
 
-    if (!isUnlocked) {
+    if (isUnlocked) {
       // return res.status(403).json({ error: 'You must unlock access to chat' });
       socket.emit('error', { error: 'You must unlock access to chat' });
     }
